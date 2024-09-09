e7f6fa866c7120fc93fbd032f3dfb25e56e14433
FernandoIvan <fascenciocortes@gmail.com>
2024-09-09 11:57:18 -0600

    Clase 2

--- app/src/main/java/com/example/voicersapp/MainActivity.java
+++ app/src/main/java/com/example/voicersapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.voicersapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+
+import com.example.voicersapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    AdaptadorNombres adaptador;
+
+    ArrayList<String> listaNombres = new ArrayList<String>(Arrays.asList("Potter","Ronald","Fulgencio","Traka","Bebote","Costas"));
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher
+            = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),result->{
+                if(result.getResultCode()== Activity.RESULT_OK){
+                    ArrayList<String> data = result.getData()!=null?
+                            result.getData().getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS):null;
+                    if(data!=null){
+                        binding.etNombre.setText(data.get(0));
+                    }
+                }
+    });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.rvNombres.setLayoutManager(new LinearLayoutManager(this));
+        adaptador = new AdaptadorNombres(listaNombres);
+        binding.rvNombres.setAdaptador(adaptador);
+
+        binding.ibtnMicrofono.setOnClickListener(
+                
+        );
     }
 }
